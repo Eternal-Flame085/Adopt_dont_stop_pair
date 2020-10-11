6d8e7523fdc526d4b8250b5988139b79feb8f5e2
Roberto Basulto <roberto.basulto085@gmail.com>
2020-10-10 21:43:50 -0400

    Created tests for all featurs in shelters

---
+++ spec/features/shelters/edit.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'As a visitor' do
+  describe 'When i visit /shelters/:id/edit' do
+    it 'Displays a form filled in in which i can change information' do
+      shelter_1 = Shelter.create(name: 'AOA', address: '6254',
+                  city: 'Miami', state: 'CH', zip: '636')
+
+      visit "/shelters/#{shelter_1.id}/edit"
+      expect(page).to have_content("Edit Shelter Info")
+
+      fill_in 'shelter[name]', with: "Heaven"
+      fill_in 'shelter[address]', with: "#{shelter_1.address}"
+      fill_in 'shelter[city]', with: "#{shelter_1.city}"
+      fill_in 'shelter[state]', with: "#{shelter_1.state}"
+      fill_in 'shelter[zip]', with: "#{shelter_1.zip}"
+
+      click_button 'Submit shelter'
+      expect(current_path).to eq('/shelters')
+
+      expect(page).to have_content("Heaven")
+      expect(page).to have_content("#{shelter_1.address}")
+      expect(page).to have_content("#{shelter_1.city}")
+      expect(page).to have_content("#{shelter_1.state}")
+      expect(page).to have_content("#{shelter_1.zip}")
+    end
+  end
+end
